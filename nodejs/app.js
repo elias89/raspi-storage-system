7d75108211cc1345d04b84a10119e8649c396847
Elias Jaime <eliasjaime.89@gmail.com>
2022-02-27 20:49:38 -0300

    rss - Testing different dir

--- nodejs/app.js
+++ nodejs/app.js
@@ -7,7 +7,7 @@ const port = 3000;
 
 
 const server = http.createServer((req, res) => {
-  var filteredTree = dirTree('/rss-files/external-hdd');
+  var filteredTree = dirTree('/media/pi/62d69d25-ae3a-4d73-84d7-2331f26a7374/Home/');
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.end(JSON.stringify(filteredTree));
